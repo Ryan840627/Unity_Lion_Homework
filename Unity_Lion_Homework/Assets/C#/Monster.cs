25e1c8b6f52f887c887b604d87f879b383edd339
Ryan840627 <89112574+Ryan840627@users.noreply.github.com>
2021-09-06 20:35:42 +0800

    完成C#功課Monster和NPC

---
+++ Unity_Lion_Homework/Assets/C#/Monster.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    [Header("Monster相關參數"),Range(0,10)]
+    public float MonsterSpeed = 3.5f;
+    [Range(0, 500)]
+    public int MonsterAttack = 100;
+    [Range(0,5000)]
+    public int MonsterHp = 350;
+    [Range(0, 50)]
+    public int monsterTrackRange = 30;
+    public Vector3 v3MonsterPosition;
+    [Header("掉落道具係數")]
+    public bool GameObject = false;
+    [Range(0, 1)]
+    public float GameObjectDrop = 1;
+    [Header("音效檔案")]
+    public AudioClip ObjectDropSound;
+    public AudioClip HurtSound;
+    public AudioClip AttackSound;
+
+    private AudioSource aud;
+    private Rigidbody2D rig;
+    private Animator ani;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity_Lion_Homework/Assets/C#/NPC.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    /// <summary>
+    /// 對話
+    /// </summary>
+    /// <param name="conversation">對話的內容</param>
+    private void Talk(string conversation) 
+    {
+
+    }
+    /// <summary>
+    /// 開啟商店
+    /// </summary>
+    /// <returns>是否開啟商店</returns>
+    public bool OpenShop() 
+    {
+        return true;
+    }
+    /// <summary>
+    /// 購買道具
+    /// </summary>
+    /// <param name="ItemMoney">道具價錢</param>
+    /// <returns></returns>
+    public int BuyItem(int ItemMoney=100) 
+    {
+        return 0;
+    }
+    /// <summary>
+    /// 取得任務
+    /// </summary>
+    /// <param name="MissonNumber">任務編號</param>
+    public void GetMisson(int MissonNumber)
+    {
+
+    }
+    /// <summary>
+    /// 任務更新
+    /// </summary>
+    /// <param name="MissonItem">獲得的任務道具</param>
+    /// <returns></returns>
+    private int MissonUpdate(int MissonItem = 1)
+    {
+        return 0;
+    }
+    /// <summary>
+    /// 完成任務
+    /// </summary>
+    /// <param name="MissonNumber">任務編號</param>
+    /// <returns></returns>
+    private bool MissonComplte(int MissonNumber)
+    {
+        return false;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
